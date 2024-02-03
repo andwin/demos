413ea6c67c5a26dd6e5ad90938453dba6e476ab1
Anders Windelhed <anders.windelhed@tv4.se>
2024-02-04 00:07:22 +0100

    Updating card design

--- src/App.svelte
+++ src/App.svelte
@@ -9,6 +9,21 @@
       title: 'Circes',
       description: 'Drawing random circles on a canvas',
       link: './src/circles/'
+    },
+    {
+      title: 'Circes',
+      description: 'Drawing random circles on a canvas',
+      link: './src/circles/'
+    },
+    {
+      title: 'Circes',
+      description: 'Drawing random circles on a canvas',
+      link: './src/circles/'
+    },
+    {
+      title: 'Circes',
+      description: 'Drawing random circles on a canvas',
+      link: './src/circles/'
     }
   ]
 </script>
@@ -18,8 +33,8 @@
 
   <div class="cards">
     {#each demos as demo}
-      <div class="card">
-        <a href={demo.link}>
+      <div>
+        <a href={demo.link} class="card">
           <h2>{demo.title}</h2>
           <p>{demo.description}</p>
           <img src={demo.link + 'preview.png'} alt={demo.title} />
@@ -28,3 +43,47 @@
     {/each}
   </div>
 </main>
+
+<style>
+  main {
+    padding: 20px;
+    color:  rgba(255, 255, 245, .86)
+  }
+
+  .cards {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 20px;
+  }
+
+  .card {
+    display: inline-block;
+    background-color: #202127;
+    padding: 20px;
+    border-radius: 10px;
+  }
+
+  h1 {
+    font-size: 3.2em;
+    padding-bottom: 60px;
+  }
+
+  h2 {
+    color: #EEE;
+    margin-top: 0;
+  }
+
+  a {
+    color: #CCC;
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+
+  img {
+    width: 100%;
+    max-width: 300px;
+    height: auto;
+  }
+</style>
