e5add19c6428ac4c1073e11df8235f09ba72f59b
Anders Windelhed <anders.windelhed@tv4.se>
2024-02-25 09:39:39 +0100

    Started complete rewrite

--- src/tunnel/index.js
+++ src/tunnel/index.js
@@ -1,8 +1,7 @@
-let sections = []
 let gfx
-let layer
-let msk
 let maxSize
+let t = 1.5
+const sectionLimit = 10
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight)
@@ -11,47 +10,29 @@ function setup() {
 
   if (gfx) gfx.remove()
   gfx = createGraphics(width, height)
-  if (layer) layer.remove()
-  layer = createGraphics(width, height)
-  if (msk) msk.remove()
-  msk = createGraphics(width, height)
-
-  sections.length = 0
-  sections.push({ color: 'green', size: 100 })
-  sections.push({ color: 'blue', size: 150 })
-  sections.push({ color: 'orange', size: 200 })
-  sections.push({ color: 'yellow', size: 250 })
-  sections.push({ color: 'red', size: 300 })
-  sections.push({ color: 'pink', size: 350 })
-  sections.push({ color: 'purple', size: 400 })
-  sections.push({ color: 'lightgreen', size: 450 })
-  sections.push({ color: 'gray', size: 500 })
 }
+
 window.onresize = setup
 
 function draw() {
   gfx.background(220)
   gfx.noFill()
 
-  for (const item of sections) {
-    const x = 50 * sin(frameCount * 0.01 + item.size * 0.01) + width / 2
-    const y = 50 * sin(frameCount * 0.01 + item.size * 0.01) + height / 2
+  t += 0.01
 
-    msk.background(item.color)
-    msk.erase()
-    msk.circle(x, y, item.size)
+  for (let section = 0; section < sectionLimit; section++) {
+    const sectionT = t - (sectionLimit - section)
+    if (sectionT < 0) continue
 
-    const mskImage = msk.get()
+    const size = ((maxSize / sectionLimit) * sectionT ** 2) / sectionLimit
 
-    const layerImage = layer.get()
-    layerImage.mask(mskImage)
+    const x = 50 * sin(frameCount * 0.01 + size * 0.01) + width / 2
+    const y = 50 * sin(frameCount * 0.01 + size * 0.01) + height / 2
 
-    gfx.image(mskImage, 0, 0)
+    gfx.circle(x, y, size)
 
-    item.size++
-    if (item.size > maxSize) {
-      item.size = 0
-      sections = sections.sort((a, b) => a.size - b.size)
+    if (size > maxSize) {
+      t -= 1
     }
   }
 
