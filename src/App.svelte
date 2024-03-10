fbed50c279f04f3e6406a1577ce041fa528518a3
Anders Windelhed <anders.windelhed@tv4.se>
2024-03-10 11:33:47 +0100

    Adding random lines project

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,10 @@
 
   const demos = [
     {
+      title: 'Random lines',
+      slug: 'random-lines',
+    },
+    {
       title: 'Quilt',
       slug: 'quilt',
     },
---
+++ src/random-lines/index.js
@@ -0,0 +1,33 @@
+import p5 from 'p5'
+
+const sizeX = 40
+const sizeY = 55
+const scale = 0.008
+
+window.setup = () => {
+  createCanvas(window.innerWidth, window.innerHeight)
+  drawLines()
+}
+window.onresize = window.setup
+window.onmousedown = () => drawLines()
+
+const drawLines = () => {
+  background(255)
+  strokeWeight(4)
+  noiseSeed()
+
+  const sizeXadjusted = width / floor(width / sizeX)
+  const sizeYadjusted = height / floor(height / sizeY)
+
+  for (let x = width - sizeXadjusted; x > -1; x -= sizeXadjusted) {
+    for (let y = height - sizeYadjusted; y > -1; y -= sizeYadjusted) {
+      const r = noise(x * scale, y * scale)
+
+      if (r > 0.5) {
+        line(x, y, x + sizeXadjusted, y + sizeYadjusted)
+      } else {
+        line(x, y + sizeYadjusted, x + sizeXadjusted, y)
+      }
+    }
+  }
+}
