8f5b3f9dbfbab115be6d9a4fdc55b4ac82f850a0
Anders Windelhed <anders.windelhed@tv4.se>
2024-03-30 17:19:10 +0100

    Resetting on touch event

--- src/fractal-tree/index.js
+++ src/fractal-tree/index.js
@@ -3,6 +3,7 @@ import p5 from 'p5'
 const bgColor = '#1A1B3E'
 let angle
 let startLength
+let t = 0
 
 window.setup = () => {
   createCanvas(window.innerWidth, window.innerHeight)
@@ -19,8 +20,8 @@ window.draw = () => {
   stroke(255)
   strokeWeight(3)
 
-  const t = frameCount
   angle = 1.02 ** -(t) * sin(0.1 * (t - 7)) + 0.45
+  t++
 
   drawBranch(width / 2, height, startLength, 0, 10)
 }
@@ -38,3 +39,9 @@ const drawBranch = (x, y, length, startAngle, level) => {
   drawBranch(endX, endY, length * 0.67, startAngle - angle, level - 1)
   drawBranch(endX, endY, length * 0.67, startAngle + angle, level - 1)
 }
+
+const resetTree = () => {
+  t = 0
+}
+
+window.touchStarted = resetTree
