578389d1a1cf0573c85d90f5749e31aede4dfa6a
Anders Windelhed <anders.windelhed@tv4.se>
2024-04-06 16:26:07 +0200

    Animating

--- src/water-surface/index.js
+++ src/water-surface/index.js
@@ -4,8 +4,9 @@ let img
 let imgWidth
 let imgHeight
 const pixelSize = 2
-const points = []
-const precalculatedPixels = []
+const initialPoints = []
+let precalculatedPixels
+const animationFrames = 30
 
 window.setup = () => {
   createCanvas(window.innerWidth, window.innerHeight)
@@ -16,34 +17,48 @@ window.setup = () => {
   const numberOfPoints = (width * height) / 50000
   for (let i = 0; i < numberOfPoints; i++) {
     const point = createVector(random(imgWidth), random(imgHeight))
-    points.push(point)
+    initialPoints.push(point)
   }
 
-  for (let x = 0; x < imgWidth; x++) {
-    for (let y = 0; y < imgHeight; y++) {
-      const distances = points.map(point => (x - point.x) ** 2 + (y - point.y) ** 2).sort((a, b) => a - b)
-      const nearestDistance = Math.sqrt(distances[0])
+  precalculatedPixels = new Array(animationFrames)
+  for (let animationFrame = 0; animationFrame < animationFrames; animationFrame++) {
+    precalculatedPixels[animationFrame] = new Array(imgWidth * imgHeight * 4)
 
-      const r = (nearestDistance / 14.5) ** 2.5 + 44
-      const g = (nearestDistance / 21) ** 2.5 + 169
-      const b = (nearestDistance / 40) ** 3.0 + 225
+    const points = initialPoints.map((p) => {
+      const x = 50 * sin(animationFrame * (TWO_PI / animationFrames) + p.x) + p.x
+      const y = 50 * cos(animationFrame * (TWO_PI / animationFrames) + p.y) + p.y
 
-      const index = (x + y * imgWidth) * 4
+      return createVector(x, y)
+    })
 
-      precalculatedPixels[index + 0] = r
-      precalculatedPixels[index + 1] = g
-      precalculatedPixels[index + 2] = b
-      precalculatedPixels[index + 3] = 255
+    for (let x = 0; x < imgWidth; x++) {
+      for (let y = 0; y < imgHeight; y++) {
+        const distances = points.map(point => (x - point.x) ** 2 + (y - point.y) ** 2).sort((a, b) => a - b)
+        const nearestDistance = Math.sqrt(distances[0])
+
+        const r = (nearestDistance / 14.5) ** 2.5 + 44
+        const g = (nearestDistance / 21) ** 2.5 + 169
+        const b = (nearestDistance / 40) ** 3.0 + 225
+
+        const index = (x + y * imgWidth) * 4
+
+        precalculatedPixels[animationFrame][index + 0] = r
+        precalculatedPixels[animationFrame][index + 1] = g
+        precalculatedPixels[animationFrame][index + 2] = b
+        precalculatedPixels[animationFrame][index + 3] = 255
+      }
     }
   }
 }
 window.onresize = window.setup
 
 window.draw = () => {
+  const animationFrame = frameCount % animationFrames
+
   img.loadPixels()
 
-  for (let i = 0; i < precalculatedPixels.length; i++) {
-    img.pixels[i] = precalculatedPixels[i]
+  for (let i = 0; i < precalculatedPixels[animationFrame].length; i++) {
+    img.pixels[i] = precalculatedPixels[animationFrame][i]
   }
 
   img.updatePixels()
