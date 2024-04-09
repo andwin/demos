72a1ada087a9e1559ee5bba966197a5d941927c3
Anders Windelhed <anders.windelhed@tv4.se>
2024-04-09 22:12:44 +0200

    Refactoring to make recursive

--- src/rotating-circles/index.js
+++ src/rotating-circles/index.js
@@ -1,31 +1,34 @@
 import p5 from 'p5'
 
+const maxLevel = 5
 let angle = 0
 
 window.setup = () => {
   createCanvas(window.innerWidth, window.innerHeight)
+  stroke(0)
+  strokeWeight(3)
 }
 window.onresize = window.setup
 
 window.draw = () => {
-  translate(width / 2, height / 2)
   background(220)
 
-  stroke(0)
-  strokeWeight(3)
+  const x = width / 2
+  const y = height / 2
+  const diameter = Math.min(width, height) * 0.95
 
-  const d = Math.min(width, height) * 0.95
-  circle(0, 0, d)
-
-  const d2 = d / 2
-  const x2 = cos(angle) * (d / 4)
-  const y2 = sin(angle) * (d / 4)
-  circle(x2, y2, d2)
-
-  const d3 = d / 4
-  const x3 = cos(angle * 2) * (d / 8)
-  const y3 = sin(angle * 2) * (d / 8)
-  circle(x2 + x3, y2 + y3, d3)
+  drawLevel(x, y, diameter, angle, 0)
 
   angle += 0.005
+}
+
+const drawLevel = (x, y, diameter, levelAngle, level) => {
+  if (level >= maxLevel) return
+
+  circle(x, y, diameter)
+
+  const x2 = cos(levelAngle) * (diameter / 4)
+  const y2 = sin(levelAngle) * (diameter / 4)
+
+  drawLevel(x + x2, y + y2, diameter / 2, angle * 2, ++level)
 }
