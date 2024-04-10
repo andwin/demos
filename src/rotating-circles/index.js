cfbeebcf756d68db61d126506334d5ca48ed6268
Anders Windelhed <anders.windelhed@tv4.se>
2024-04-10 23:08:43 +0200

    Adding colors

--- src/rotating-circles/index.js
+++ src/rotating-circles/index.js
@@ -2,6 +2,15 @@ import p5 from 'p5'
 
 const maxLevel = 5
 let angle = 0
+const backgroundColor = '#FFFFFF'
+const colors = {
+  0: '#FF0000',
+  1: '#00FF00',
+  2: '#0000FF',
+  3: '#FFFF00',
+  4: '#FF00FF',
+  5: '#00FFFF',
+}
 
 window.setup = () => {
   createCanvas(window.innerWidth, window.innerHeight)
@@ -11,7 +20,7 @@ window.setup = () => {
 window.onresize = window.setup
 
 window.draw = () => {
-  background(220)
+  background(backgroundColor)
 
   const x = width / 2
   const y = height / 2
@@ -25,6 +34,7 @@ window.draw = () => {
 const drawLevel = (x, y, diameter, levelAngle, level) => {
   if (level >= maxLevel) return
 
+  fill(colors[level])
   circle(x, y, diameter)
 
   const x2 = cos(levelAngle) * (diameter / 4)
