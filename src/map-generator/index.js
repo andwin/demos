ec4f65cba417f945ce4690f803e9d80dbd65c4cf
Anders Windelhed <anders.windelhed@tv4.se>
2024-05-08 23:52:27 +0200

    Fading colors

--- src/map-generator/index.js
+++ src/map-generator/index.js
@@ -7,11 +7,30 @@ const noiseScale = 0.008
 let t = 100.0
 let graphics
 
+let colors
+
 window.setup = () => {
   createCanvas(window.innerWidth, window.innerHeight)
 
   graphics = createGraphics(window.innerWidth, window.innerHeight)
   graphics.noStroke()
+
+  const deepWater = color('#223058')
+  const shallowWater = color('#87A4E8')
+  const beach = color(240, 211, 158)
+  const grass = color('#55CB15')
+  const wood = color('#38850E')
+  const mountain = color(200, 200, 200)
+  const snow = color(255)
+
+  colors = [
+    { fromValue: 0, toValue: 0.4, fromColor: deepWater, toColor: shallowWater },
+    { fromValue: 0.4, toValue: 0.45, fromColor: shallowWater, toColor: beach },
+    { fromValue: 0.45, toValue: 0.5, fromColor: beach, toColor: grass },
+    { fromValue: 0.5, toValue: 0.6, fromColor: grass, toColor: wood },
+    { fromValue: 0.6, toValue: 0.8, fromColor: wood, toColor: mountain },
+    { fromValue: 0.8, toValue: 1, fromColor: mountain, toColor: snow },
+  ]
 
   initGraphics(graphics)
 }
@@ -53,20 +72,12 @@ const drawPixel = (gfx, x, y) => {
 }
 
 const colorForNoiseValue = (noiseValue) => {
-  switch (true) {
-    case (noiseValue < 0.3): // deep water
-      return color(87, 87, 171)
-    case (noiseValue < 0.4): // shallow water
-      return color(115, 115, 255)
-    case (noiseValue < 0.45): // beach
-      return color(240, 211, 158)
-    case (noiseValue < 0.5): // grass
-      return color(101, 235, 134)
-    case (noiseValue < 0.6): // wood
-      return color(72, 138, 89)
-    case (noiseValue < 0.80): // mountain
-      return color(200, 200, 200)
-    default: // snow
-      return color(255)
+  const terrain = colors.find(color => noiseValue >= color.fromValue && noiseValue < color.toValue)
+
+  if (!terrain) {
+    return color(255)
   }
+
+  const lerpValue = map(noiseValue, terrain.fromValue, terrain.toValue, 0, 1)
+  return lerpColor(terrain.fromColor, terrain.toColor, lerpValue)
 }
